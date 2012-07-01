b4cbfd7130b132446e54c8526f15ff1686cfab5b
Mami <Mami@mami-d9fb25f084>
2012-07-01 16:17:34 +0200

    najnowsza wersja

---
+++ src/com/android/kontakty/GetResults.java
@@ -0,0 +1,33 @@
+package com.android.kontakty;
+
+public class GetResults {
+
+		 private String layoutName = "";
+		 private String layoutID = "";
+		 private String LayoutPhone = "";
+
+		 public void setLayoutName(String name) {
+		  this.layoutName = name;
+		 }
+
+		 public String getLayoutName() {
+		  return layoutName;
+		 }
+
+		 public void setLayoutID(String id) {
+		  this.layoutID = id;
+		 }
+
+		 public String getLayoutID() {
+		  return layoutID;
+		 }
+
+		 public void setLayoutPhone(String phone) {
+		  this.LayoutPhone = phone;
+		 }
+
+		 public String getLayoutPhone() {
+		  return LayoutPhone;
+		 }
+		
+}
--- src/com/android/kontakty/MainActivity.java
+++ src/com/android/kontakty/MainActivity.java
@@ -2,16 +2,45 @@ package com.android.kontakty;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener {
+	SimpleCursorAdapter adapter;
+	ListView listaKontaktow;
+	TextView rowText;
+	String[] cos;
+	
+  
 
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaKontaktow = (ListView)findViewById(R.id.listaKontaktow);
+
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[] {Phone._ID, Phone.DISPLAY_NAME, Phone.NUMBER}, null, null, null);
+
+        //startManagingCursor(cursor);
+        
+        String[] from = new String[] { Phone.DISPLAY_NAME};
+        int[] to = new int[] { R.id.layoutName };
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,R.layout.rowfromlist,cursor,from,to);
+        
+        listaKontaktow.setAdapter(adapter);
+        //cos = (String[])adapter.getItem(0);
+        listaKontaktow.setOnItemClickListener(this);
+        
     }
 
     @Override
@@ -30,6 +59,26 @@ public class MainActivity extends Activity {
 		}
 		return true;
 	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		//listaKontaktow.getId()
+//		String[] cos = (String[])listaKontaktow.getAdapter().getItem(arg2);
+//		String[] tmp = (String[]) arg0.getItemAtPosition(arg2);
+		//rowText = (TextView)findViewById(R.id.textView1);
+		//TextView rowText2 = (TextView)arg0.getCount();
+	
+		//long[] map = listaKontaktow.getCheckItemIds();
+		//TextView rowText2 = (TextView)arg0.getItemAtPosition(arg2);
+		//String s = arg0.getItemAtPosition(arg2).toString();
+		//rowText.setText(s);	
+//		rowText.setText(cos[0]);	
+	}
+	
+
+
+
+	
 
     
 }
